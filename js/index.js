917d9a4e0d28962c4cef74f435c50b37eb071763
ApoloLB <apololbastos@gmail.com>
2024-09-03 17:49:23 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+function mostrarAlerta(event) {
+    event.preventDefault(); // Evita o envio do formulário para mostrar o alerta primeiro
+    alert("Ainda estou trabalhando o Formulário! Tente novamente mais tarde!");
+}
