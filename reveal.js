ffde6036170abdc1b7a1394e41096f127a523b54
ApoloLB <apololbastos@gmail.com>
2025-02-21 17:33:55 -0300

    reveal

---
+++ reveal.js
@@ -0,0 +1,25 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.scroll-reveal', {
+    duration:2000,
+    distance:'90px'
+})
+
+revelar.reveal('.scroll-reveal-img', {
+    duration:2000,
+    distance:'90px',
+    delay: 500
+})
+
+revelar.reveal('.scroll-reveal-skill', {
+    duration: 1000,
+    distance:'90px',
+    
+})
+
+
+
+
+
+
