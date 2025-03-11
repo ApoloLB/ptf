1b93486143b96a3736f5e38c13d3c2309d0ea4f1
ApoloLB <apololbastos@gmail.com>
2025-03-11 20:20:18 -0300

    Finalizando

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ let close = document.getElementById('closeMenu')
 
 btnOpen.addEventListener('click',()=>{
     mobile.classList.add('openMenu')
+    
 })
 
 closeMenu.addEventListener('click',()=>{
     mobile.classList.remove('openMenu')
+    
+})
+
+btnOpen.addEventListener('click', ()=>{
+    btnOpen.classList.add('invisible')
+})
+
+closeMenu.addEventListener('click', ()=>{
+    btnOpen.classList.remove('invisible')
 })
